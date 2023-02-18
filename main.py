c150de9d916d9574cedc4606473fe6726ec62dbf
MohammedYasserr <bmbmyasser@gmail.com>
2023-02-19 00:17:22 +0200

    changing variable name from read to sequence to be more meangingful

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ import gzip
 import pyfastx
 
 #function to calculate how many sequences(reads) in one fastq.gz file 
-
 def get_seq_count():
-    reads=0
-    with gzip .open('./data/test.fastq.gz' ,'rb' ) as read:
-        for id in read:
-            seq = next(read)
-            reads +=1
-            next(read)
-            next(read)
-    print(reads)
+    sequences = 0
+    with gzip .open('./data/test.fastq.gz' ,'rb' ) as sequence:
+        for id in sequence:
+            seq = next(sequence)
+            sequences +=1
+            next(sequence)
+            next(sequence)
+    print(sequences)
 
 get_seq_count()
 
