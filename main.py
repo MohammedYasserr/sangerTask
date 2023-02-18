fc1a2f1f46e51ecd43b764e95512b66fe4b84212
MohammedYasserr <bmbmyasser@gmail.com>
2023-02-19 00:14:18 +0200

    setting up the function that calculates the number of sequences in one compressed fastq file

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 print("Hello Sanger")
+
+import gzip
+import pyfastx
+
+#function to calculate how many sequences(reads) in one fastq.gz file 
+
+def get_seq_count():
+    reads=0
+    with gzip .open('./data/test.fastq.gz' ,'rb' ) as read:
+        for id in read:
+            seq = next(read)
+            reads +=1
+            next(read)
+            next(read)
+    print(reads)
+
+get_seq_count()
+
+
