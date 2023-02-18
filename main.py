0974ba7400b853ef03032e603b58ffe8a71ba622
MohammedYasserr <bmbmyasser@gmail.com>
2023-02-19 00:21:25 +0200

    adding the function that get the number of nucleotides in one fastq file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 print("Hello Sanger")
 
 import gzip
-import pyfastx
 
 #function to calculate how many sequences(reads) in one fastq.gz file 
 def get_seq_count():
@@ -17,3 +16,16 @@ def get_seq_count():
 get_seq_count()
 
 
+# function to calculate how many nucleotides in one fastq.gz file
+def get_nucleotides_count():
+    bases = 0
+    with gzip .open('./data/test.fastq.gz', 'rb') as read:
+        for id in read:
+            seq = next(read)
+            bases += len(seq.strip())
+            next(read)
+            next(read)
+    print(bases)
+
+
+get_nucleotides_count()
